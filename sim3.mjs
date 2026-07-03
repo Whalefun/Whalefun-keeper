40fbbb306400bcaefcc7e17804de907b484115fb
Whalefun <289755659+Whalefun@users.noreply.github.com>
2026-07-03 19:55:18 +0800

    Create sim3.mjs

---
+++ sim3.mjs
@@ -0,0 +1,14 @@
+import { ethers } from 'ethers'
+const p = new ethers.JsonRpcProvider('https://bsc-dataseed.bnbchain.org')
+const LF='0xB5AF6387ed653F3f15C01Da4031571Fd454DF22f', NFTF='0x5542FD41EcD84090Db760Dc9cc73e5A15dd6DAbf'
+const ME='0x688b63F94dda23986aa5CD1398D4b2c052888Dc0', Z=ethers.ZeroAddress
+const clFrag={"type":"function","name":"createLaunch","stateMutability":"payable","inputs":[{"name":"cfg","type":"tuple","components":[{"name":"name","type":"string"},{"name":"symbol","type":"string"},{"name":"totalSupply","type":"uint256"},{"name":"mintCount","type":"uint256"},{"name":"unitPriceBNB","type":"uint256"},{"name":"buyTaxBps","type":"uint16"},{"name":"sellTaxBps","type":"uint16"},{"name":"bucketVaultBps","type":"uint16"},{"name":"bucketMarketingBps","type":"uint16"},{"name":"bucketLiquidityBps","type":"uint16"},{"name":"bucketDividendBps","type":"uint16"},{"name":"bucketBurnBps","type":"uint16"},{"name":"marketingWallet","type":"address"},{"name":"quoteToken","type":"address"},{"name":"airdropEnabled","type":"bool"},{"name":"airdropCount","type":"uint8"},{"name":"dividendToken","type":"address"},{"name":"minimumShareBalance","type":"uint256"}]},{"name":"vaultFactory","type":"address"},{"name":"vaultData","type":"bytes"},{"name":"salt","type":"bytes32"}],"outputs":[]}
+const iface=new ethers.Interface([clFrag])
+const vaultData=ethers.AbiCoder.defaultAbiCoder().encode(['tuple(string,string,uint256,uint256,uint256,uint16,uint16,string)'],[['LABUBU','LABUBU',300n,300n,ethers.parseEther('2000'),90,10,'ipfs://bafybeifrv77itkwm5jlay6ubdu6nx7bw4rp4ksseei57p3dbydsuu2sp7i/']])
+const cfg=['LABUBU','LABUBU',ethers.parseEther('1000000'),200n,ethers.parseEther('0.01'),300,300,8000,0,500,0,1500,ME,Z,false,0,Z,0n]
+const salt='0x00000000000000000000000000000000000000000000000000000000abcd0001'
+const data=iface.encodeFunctionData('createLaunch',[cfg,NFTF,vaultData,salt])
+try {
+  const g = await p.estimateGas({to:LF,from:ME,value:ethers.parseEther('0.005'),data})
+  console.log('createLaunch 实际需要 gas:', g.toString())
+} catch(e){ console.log('estimateGas 失败:', e.reason||e.shortMessage||e.message) }
